4eee593c27f3e6c93b756d68f1896f532a19daa6
Roggers  Anguzu <ranguzu@usiu.ac.ke>
2026-02-11 11:54:19 +0300

    Verification Stage 1

--- routes/livestockRoutes.js
+++ routes/livestockRoutes.js
@@ -6,20 +6,4 @@ router.get("/priceCalculator", (req, res) => {
     title: "Animal Price Calculator",
   });
 });
-router.get("/verification", (req, res) => {
-  res.render("livestock/verification", {
-    title: "Upload Data for Verification",
-  });
-});
-router.get("/notes", (req, res) => {
-  res.render("livestock/notes", {
-    title: "Important Notes",
-  });
-});
-router.get("/settings", (req, res) => {
-  res.render("livestock/settings", {
-    title: "Settings",
-  });
-});
-
 export default router;
